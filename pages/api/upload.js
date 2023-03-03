f83d2df4c0fb28d9442a540dfa0e057f4a121d99
kanno-takumi <bp20065@shibaura-it.ac.jp>
2023-03-04 02:39:14 +0900

    firebaseへのアップデート

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -5,14 +5,17 @@ import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 //single image file upload
 export const postImage = async(image=null) => {
     let uploadResult = '';
- 
+    console.log("aaaaaaa");
     if(image.name){
+        console.log("bbbbbb");
         const storageRef = ref(storage);
         const ext = image.name.split('.').pop();
         const hashName = Math.random().toString(36).slice(-8);
         const fullPath = '/images/' + hashName + '.' + ext;
         const uploadRef = ref(storageRef, fullPath);
  
+        console.log(image.name);
+        console.log(uploadRef)
         // 'file' comes from the Blob or File API
         await uploadBytes(uploadRef, image).then(async function(result) {
             console.log(result);
