a7e42a9a54c0adc24b5b952420011abe951f87a4
kanno-takumi <bp20065@shibaura-it.ac.jp>
2023-04-03 23:59:48 +0900

    a

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
+import { getFirestore, collection, getDocs,query,where,addDoc,onSnapshot,doc,getDoc} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,7 +21,41 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db =  getFirestore(app);
 // const analytics = getAnalytics(app);
-export const storage=getStorage(app);
- 
-console.log(app);
+
+// const storage=getStorage(app);
+
+export async function getCafeData() {//promiseオブジェクトを返す //1つのcafe
+  const docRef = doc(db,'cafelists','all');
+  const docSnap =await getDoc(docRef)
+  // const querySnapshot = await getDocs(col);//コメダ、スタバなどの名前=documents
+  const cafeData=docSnap.data();
+  console.log(cafeData)
+  // const cafesData = Object 
+  // const cafesData = Object.entries(cafeData)
+  // console.log(cafesData);
+  console.log("tamesi")
+  return cafeData
+  // return postsArray
+}
+
+export async function getArrangedData(){
+  const allCafeData = await getCafeData();
+  const cafesData = Object.entries(allCafeData)
+  console.log(cafesData)
+  return cafesData
+}
+
+export async function addCafeData(postData){
+  try {
+    const docRef = await addDoc(collection(db, "posts"), {
+      content:postData.content,
+      date: postData.date,
+      title: postData.title
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
